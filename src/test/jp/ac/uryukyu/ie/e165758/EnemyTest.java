268ef5c8b5e48d156eafa3defbfb779e1210100c
Okuto Matsuda <e165758@ie.u-ryukyu.ac.jp>
2016-11-25 22:25:54 +0900

    プログラミング2,　レポート４

--- src/test/jp/ac/uryukyu/ie/e165758/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165758/EnemyTest.java
@@ -13,6 +13,7 @@ public class EnemyTest {
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 3, 3);
+        enemy.dead = true;
         for (int i = 0; i < 3; i++) {
             enemy.attack(hero);
         }
